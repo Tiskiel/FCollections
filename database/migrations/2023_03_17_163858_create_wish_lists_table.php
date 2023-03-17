746f924cb195cce3970071deade127a2ff0ca0e5
Jean-François <jf@Jean-Francoiss-MacBook-Air.local>
2023-03-17 22:27:00 +0100

    add forign key of user to wishlist

--- database/migrations/2023_03_17_163858_create_wish_lists_table.php
+++ database/migrations/2023_03_17_163858_create_wish_lists_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('wish_lists', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onUpdate('cascade');
+            $table->string('title');
+            $table->string('body');
+            $table->string('slug');
             $table->timestamps();
         });
     }
