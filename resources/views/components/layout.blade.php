6734e712aeb6590fa1199991206200c9a454d382
Jean-François <jf@Jean-Francoiss-MacBook-Air.local>
2023-03-17 17:57:15 +0100

    First commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    {{-- Variable $slot utilisée pour communiquer avec x-layout --}}
+    {{ $slot }}
+</body>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1>Start FCollections laravel</h1>
+</x-layout>
