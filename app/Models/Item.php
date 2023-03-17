f92f79e2f8ee567446eafccc4852565208180210
Jean-François <jf@Jean-Francoiss-MacBook-Air.local>
2023-03-17 23:15:26 +0100

    add eager loading to item

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -13,11 +14,17 @@ class Item extends Model
 
     protected $guarded = [];
 
+    protected $with = ['category'];
+
     public function wishlist() : BelongsToMany {
         return $this->belongsToMany(WishList::class);
     }
 
     public function userlist() : BelongsToMany {
         return $this->belongsToMany(UserList::class);
+    }
+
+    public function category() : BelongsTo {
+        return $this->belongsTo(Category::class);
     }
 }
