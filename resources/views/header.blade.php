22972be547e989a4a078df83c3b4507699a99467
Jean-François <jf@Jean-Francoiss-MacBook-Air.local>
2023-03-18 16:55:36 +0100

    create header with tailwind css and integrate it to layout

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,3 +1,5 @@
-<header class="w-screen border border-r-4 h-20">
-        <div>Friends Collections</div>
+<header class="flex justify-evenly w-screen border border-r-4 h-20">
+    <div class="flex items-center h-20">
+        <h1 class="text-2xl mx-auto">Friends Collections</h1>
+    </div>
 </header>
