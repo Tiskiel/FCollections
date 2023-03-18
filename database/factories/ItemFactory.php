2aaf0097c59d9bbe92499711806f1916f4346b5c
Jean-François <jf@Jean-Francoiss-MacBook-Air.local>
2023-03-18 08:09:18 +0100

    create item factory and add unique to category factory

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,11 @@ class ItemFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'category_id' => Category::factory(),
+            'name' => $this->faker->unique()->word,
+            'slug' => $this->faker->unique()->word,
+            'origin' => $this->faker->countryCode,
+            'state' => $this->faker->word
         ];
     }
 }
