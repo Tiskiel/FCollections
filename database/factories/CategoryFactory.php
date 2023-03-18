abad44d7154f99e69d40d353d63dedf2398855d7
Jean-François <jf@Jean-Francoiss-MacBook-Air.local>
2023-03-18 08:00:35 +0100

    create category factory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -17,7 +17,8 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->colorName,
+            'slug' => $this->faker->slug,
         ];
     }
 }
